cbbc2fd294fb66a9c44517c935ff3cf6205511e7
ANANDA RIZKY Y <kiky.lenovo@gmail.com>
2019-12-12 12:34:33 +0700

    sclicing home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,7 +6,8 @@ import {
   Dimensions,
   TextInput,
   Image,
-  TouchableOpacity
+  TouchableOpacity,
+  ActivityIndicator
 } from 'react-native';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
@@ -32,27 +33,18 @@ class Home extends Component {
   //   title: 'Home'
   // });
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.refs.map.animateToRegion(
-        {
-          latitude: -6.3302921,
-          longitude: 106.6778804,
-          latitudeDelta: 0.02,
-          longitudeDelta: 0.02
-        },
-        2000
-      );
-    }, 2000);
-  }
-
   static navigationOptions = {
     header: null
   };
 
   state = {
     isMapReady: false,
-    image: ''
+    image: '',
+    inputLocation: '',
+    inputDetailLocation: '',
+    inputProblem: '',
+    status: 0,
+    isLoading: false
   };
 
   onImageClick = async () => {
@@ -67,8 +59,23 @@ class Home extends Component {
     });
   };
 
-  onMapLayout = () => {
-    this.setState({ isMapReady: true });
+  onChangeLayout = async () => {
+    // this.setState({ status: 1 });
+    // await this.setState({ isLoading: true });
+    // await this.setState({ isLoading: false });
+
+    await this.refs.map.animateToRegion(
+      {
+        latitude: -6.3302921,
+        longitude: 106.6778804,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02
+      },
+      2000
+    );
+    await this.refs.bottomSheet.setBottomSheetState(
+      BottomSheetBehavior.STATE_COLLAPSED
+    );
   };
 
   render() {
@@ -85,7 +92,6 @@ class Home extends Component {
           showsCompass
           provider={PROVIDER_GOOGLE} // remove if not using Google Maps
           style={styles.container}
-          onLayout={this.onMapLayout}
           region={{
             latitude: -6.175392,
             longitude: 106.827153,
@@ -115,78 +121,191 @@ class Home extends Component {
           peekHeight={250}
           hideable={false}
           state={BottomSheetBehavior.STATE_COLLAPSED}>
-          <View style={{ height: height, backgroundColor: '#fff' }}>
-            <FontAwesome5
-              style={styles.icon}
-              name="grip-lines"
-              size={18}
-              color="#c9c9c9"
-            />
-            <View style={styles.miniContainer}>
-              <Text style={styles.textTitle}>Where are you ?</Text>
-              <View style={[styles.wrapperForm, styles.wrapperDetailLocation]}>
-                <TextInput
-                  style={styles.input}
-                  placeholder="Input your location"
-                  onFocus={() =>
-                    this.refs.bottomSheet.setBottomSheetState(
-                      BottomSheetBehavior.STATE_EXPANDED
-                    )
-                  }
-                />
+          {this.state.status == 0 ? (
+            <View style={{ height: height, backgroundColor: '#fff' }}>
+              <FontAwesome5
+                style={styles.icon}
+                name="grip-lines"
+                size={18}
+                color="#c9c9c9"
+              />
+              <View style={styles.miniContainer}>
+                <Text style={styles.textTitle}>Where are you ?</Text>
+                <View
+                  style={[styles.wrapperForm, styles.wrapperDetailLocation]}>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Input your location"
+                    onFocus={() =>
+                      this.refs.bottomSheet.setBottomSheetState(
+                        BottomSheetBehavior.STATE_EXPANDED
+                      )
+                    }
+                    onChangeText={text =>
+                      this.setState({ inputLocation: text })
+                    }
+                    value={this.state.inputLocation}
+                  />
+                </View>
+                <View
+                  style={[styles.wrapperForm, styles.wrapperDetailLocation]}>
+                  <FontAwesome5
+                    style={styles.iconInput}
+                    name="edit"
+                    color="grey"
+                    size={16}
+                  />
+                  <TextInput
+                    style={styles.miniInput}
+                    placeholder="Input detail location"
+                    onFocus={() =>
+                      this.refs.bottomSheet.setBottomSheetState(
+                        BottomSheetBehavior.STATE_EXPANDED
+                      )
+                    }
+                    onChangeText={text =>
+                      this.setState({ inputDetailLocation: text })
+                    }
+                    value={this.state.inputDetailLocation}
+                  />
+                </View>
               </View>
-              <View style={[styles.wrapperForm, styles.wrapperDetailLocation]}>
-                <FontAwesome5
-                  style={styles.iconInput}
-                  name="edit"
-                  color="grey"
-                  size={16}
-                />
-                <TextInput
-                  style={styles.miniInput}
-                  placeholder="Input detail location"
-                  onFocus={() =>
-                    this.refs.bottomSheet.setBottomSheetState(
-                      BottomSheetBehavior.STATE_EXPANDED
-                    )
-                  }
-                />
+              <View style={styles.miniContainer}>
+                <Text style={styles.textTitle}>Whats your problem ?</Text>
+                <View
+                  style={[styles.wrapperForm, styles.wrapperDetailLocation]}>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Input your problem"
+                    onFocus={() =>
+                      this.refs.bottomSheet.setBottomSheetState(
+                        BottomSheetBehavior.STATE_EXPANDED
+                      )
+                    }
+                    onChangeText={text => this.setState({ inputProblem: text })}
+                    value={this.state.inputProblem}
+                  />
+                </View>
+              </View>
+              <View style={styles.miniContainer}>
+                <Text style={styles.textTitle}>Post a Picture!</Text>
+                <TouchableOpacity
+                  style={styles.image}
+                  onPress={this.onImageClick}>
+                  {image ? (
+                    <Image source={{ uri: image }} style={styles.image} />
+                  ) : (
+                    <View style={styles.image}>
+                      <FontAwesome5 name="camera" color="grey" size={24} />
+                    </View>
+                  )}
+                </TouchableOpacity>
+              </View>
+              <View style={styles.miniContainer}>
+                <TouchableOpacity
+                  onPress={async () => {
+                    await this.setState({ isMapReady: true, status: 1 });
+                    await this.onChangeLayout();
+                  }}
+                  style={styles.button}>
+                  {this.state.isLoading ? (
+                    <ActivityIndicator
+                      style={{ margin: 16 }}
+                      size="small"
+                      color="#FFF"
+                    />
+                  ) : (
+                    <Text style={styles.textButton}>Submit</Text>
+                  )}
+                </TouchableOpacity>
               </View>
             </View>
-            <View style={styles.miniContainer}>
-              <Text style={styles.textTitle}>Whats your problem ?</Text>
-              <View style={[styles.wrapperForm, styles.wrapperDetailLocation]}>
-                <TextInput
-                  style={styles.input}
-                  placeholder="Input your problem"
-                  onFocus={() =>
-                    this.refs.bottomSheet.setBottomSheetState(
-                      BottomSheetBehavior.STATE_EXPANDED
-                    )
-                  }
-                />
+          ) : (
+            <View style={{ height: height, backgroundColor: '#fff' }}>
+              <FontAwesome5
+                style={styles.icon}
+                name="grip-lines"
+                size={18}
+                color="#c9c9c9"
+              />
+              <View style={styles.miniContainer}>
+                <Text
+                  style={{ fontSize: 14, fontWeight: 'bold', color: 'black' }}>
+                  {this.state.inputLocation}
+                </Text>
               </View>
-            </View>
-            <View style={styles.miniContainer}>
-              <Text style={styles.textTitle}>Post a Picture!</Text>
-              <TouchableOpacity
-                style={styles.image}
-                onPress={this.onImageClick}>
-                {image ? (
-                  <Image source={{ uri: image }} style={styles.image} />
-                ) : (
-                  <View style={styles.image}>
-                    <FontAwesome5 name="camera" color="grey" size={24} />
+              <View style={styles.miniContainer}>
+                <View
+                  style={[
+                    styles.wrapperForm,
+                    styles.wrapperDetailLocation,
+                    { marginTop: -10 }
+                  ]}>
+                  <FontAwesome5
+                    style={styles.iconInput}
+                    name="edit"
+                    color="grey"
+                    size={16}
+                  />
+                  <TextInput
+                    editable={false}
+                    style={styles.miniInput}
+                    placeholder="Input detail location"
+                    onChangeText={text =>
+                      this.setState({ inputDetailLocation: text })
+                    }
+                    value={this.state.inputDetailLocation}
+                  />
+                </View>
+              </View>
+              <View style={styles.miniContainer}>
+                <View
+                  style={{
+                    height: 120,
+                    width: '100%',
+                    justifyContent: 'flex-end'
+                  }}>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      margin: 8,
+                      alignItems: 'center'
+                    }}>
+                    <FontAwesome5
+                      name="user-alt"
+                      solid
+                      color="black"
+                      size={24}
+                    />
+                    <Text
+                      style={{
+                        fontSize: 14,
+                        fontWeight: 'bold',
+                        color: 'black',
+                        marginLeft: 16
+                      }}>
+                      Your name goes here
+                    </Text>
                   </View>
-                )}
-              </TouchableOpacity>
-            </View>
-            <View style={styles.miniContainer}>
-              <View style={styles.button}>
-                <Text style={styles.textButton}>Submit</Text>
+                </View>
+              </View>
+              <View
+                style={[
+                  styles.miniContainer,
+                  { borderTopWidth: 2, borderTopColor: '#c9c9c9' }
+                ]}>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    fontWeight: 'bold',
+                    color: Color.tertiary,
+                    margin: 16
+                  }}>
+                  Maintenance Progress
+                </Text>
               </View>
             </View>
-          </View>
+          )}
         </BottomSheetBehavior>
       </CoordinatorLayout>
     );
